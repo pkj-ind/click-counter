2c1642536e3d3af5ce0b6527b71f7cd0f2a14a7c
pkj-ind <pramodix@gmail.com>
2020-11-28 17:22:02 +0530

    test for data-test, classname for rendered Compo

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import Enzyme, {shallow} from 'enzyme'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// setting up enzyme's react adapter
+
+Enzyme.configure({ adapter: new Adapter() });
+
+test("renders without error", ()=>{
+const wrapper=shallow(<App />)
+const appComponent = wrapper.find("[data-test='component-app']")
+expect(appComponent.length).toBe(1)
+})
+
+test("renders component with Class App", ()=>{
+  const wrapper=shallow(<App />)
+  
+  expect(wrapper.find(".App").getElements().length).toBe(1) // or
+  expect(wrapper.find("div.App").length).toBe(1)
+  expect(wrapper.find(".header").length).toBe(1) // or
+  expect(wrapper.find("h1.header").length).toBe(1)
+  })
+
+test("renders button", ()=>{
+  
+})
+test("renders counter display", ()=>{
+  
+})
+test("Counters start at Zero", ()=>{
+  
+})
+test("clicking on button increments counter display", ()=>{
+  
+})
